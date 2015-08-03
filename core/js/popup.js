3d5e458413c2f22593bd7a2a98f715250ea6ea36
andrewholsted <andrew@fajitanachos.com>
2015-08-03 12:01:30 -0600

    Remove background page and content script
    Load everythign in the popup for now

--- core/js/popup.js
+++ core/js/popup.js
@@ -2,6 +2,10 @@ var addressInput = document.getElementById('address-input');
 var nameInput = document.getElementById('name-input');
 var autocomplete = new google.maps.places.Autocomplete(addressInput);
 var savedLocations = document.getElementById('saved-locations');
+var locationsList = document.getElementById('locations-list');
+var directionsService = new google.maps.DirectionsService();
+var routes = [];
+var locations = [];
 
 function attachEventListeners() {
   google.maps.event.addListener(autocomplete, 'place_changed', function() {
@@ -19,67 +23,202 @@ function attachEventListeners() {
     }
 
     saveLocation(location);
+    resetInputs();
   });
 }
 
+function resetInputs(){
+  setTimeout(function(){
+     nameInput.value = '';
+    addressInput.value = '';
+    addressInput.blur();
+  }, 0)
+ 
+}
+
 function saveLocation(location){
-   chrome.runtime.sendMessage({name: 'saveLocation', data: location}, function(response){
-      addToSavedLocations(location);
-   });
+  locations.push(location);
+
+  chrome.storage.sync.set({'locations': locations}, function(data) {
+    showSavedLocations();
+    createLoadingItem('Calculating route...');
+    calculateRouteForLocations([location]);
+  });
 }
 
 function getLocations(){
    chrome.storage.sync.get('locations', function(data){
     if(data && data.locations && data.locations.length){
-      for(var i = 0; i < data.locations.length; i++){
-        addToSavedLocations(data.locations[i]);
-      }
+      showSavedLocations();
+      createLoadingItem('Calculating routes...');
+
+      locations = data.locations;
+      calculateRouteForLocations(data.locations);
     }
   });
 }
 
-function addToSavedLocations(location){
+function createLoadingItem(text){
+  var loadingItem = document.createElement('li');
+  loadingItem.className = 'saved-location';
+  loadingItem.id = 'location-loader';
 
+  var loadingText = document.createTextNode(text);
+
+  loadingItem.appendChild(loadingText);
+  locationsList.appendChild(loadingItem);
+}
+
+function removeLoadingItem(){
+  var loadingId = 'location-loader';
+  var loadingItem = document.getElementById(loadingId);
+
+  if(loadingItem){
+    locationsList.removeChild(loadingItem);
+  }
+
+}
+
+function showSavedLocations(){
+  console.log(savedLocations);
+  savedLocations.classList.remove('hide');
+}
+
+function addToSavedLocations(route){
+  
   var locationItem = document.createElement('li');
   locationItem.className = 'saved-location';
-  locationItem.id = location.label + '-location';
+  locationItem.id = formatRouteId(route.location.label)
 
-  var locationLabel = document.createElement('label');
+  // Route Link that shows the time
+  var routeLink = document.createElement('a');
+  routeLink.className = 'route-link';
+  routeLink.target = '_blank';
+
+  var startAddress = route.data.legs[0].start_address;
+  var endAddress = route.data.legs[0].end_address;
+
+  routeLink.href = 'https://maps.google.com?saddr='+startAddress+'&daddr='+endAddress;
+
+  var duration = route.data.legs[0].duration.text;
+  var durationText = document.createTextNode(duration);
+
+  routeLink.appendChild(durationText);
+
+  locationItem.appendChild(routeLink);
+
+  // Name of the location, and remove icon
+  var locationContainer = document.createElement('div');
+  locationContainer.className = 'location-container';
+
+  var locationLabel = document.createElement('span');
+  var labelText = document.createTextNode(route.location.label);
+
   locationLabel.className = 'location-label';
+  locationLabel.appendChild(labelText);
 
-  var labelText = document.createTextNode(location.label);
+  locationContainer.appendChild(locationLabel);
 
   var locationName = document.createElement('span');
+  var nameText = document.createTextNode(route.location.name);
+  
   locationName.className = 'location-name';
-
-  var nameText = document.createTextNode(location.name);
   locationName.appendChild(nameText);
+
+  locationContainer.appendChild(locationName);
 
   var removeIcon = document.createElement('span');
   removeIcon.className = 'remove-icon';
 
   removeIcon.addEventListener('click', function(){
-    removeLocation(location);
+    removeLocation(route.location);
   });
 
-  locationLabel.appendChild(labelText);
-  locationLabel.appendChild(locationName);
-  locationLabel.appendChild(removeIcon);
 
-  locationItem.appendChild(locationLabel);
+  locationContainer.appendChild(removeIcon);
 
-  savedLocations.appendChild(locationItem);
+  locationItem.appendChild(locationContainer);
+
+  locationsList.appendChild(locationItem);
 
 }
 
 function removeLocation(location){
-
-  var locationItem = document.getElementById(location.label + '-location');
+  var locationId = formatRouteId(location.label);
+  var locationItem = document.getElementById(locationId);
   
-  chrome.runtime.sendMessage({name: 'deleteLocation', data: location}, function(response){
-    savedLocations.removeChild(locationItem);
- });
+  for(var i = locations.length -1; i >= 0; i--){
+    if(locations[i].label === location.label){
+      locations.splice(i, 1);
+    }
+  }
 
+  chrome.storage.sync.set({'locations': locations}, function(data) {
+    locationsList.removeChild(locationItem);
+  }); 
+
+}
+
+function formatRouteId(label){
+  return label.toLowerCase().replace(/\s/g, '-') + '-route';
+}
+
+function calculateRouteForLocations(locations){
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+  }
+
+  function successFunction(position) {
+
+    var start = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+
+    for(var i = 0; i < locations.length; i++){
+
+      var end = new google.maps.LatLng(locations[i].coordinates.A, locations[i].coordinates.F);
+
+      calculateRoute(locations[i], start, end);
+    }
+  }
+
+  function errorFunction(err){
+    console.log('could not determine location')
+  }
+}
+
+function calculateRoute(location, start, end){
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: google.maps.TravelMode.DRIVING
+  };
+
+  directionsService.route(request, function(result, status) {
+    if(result && result.routes.length) {
+      route = {
+        location: location,
+        data: result.routes[0]
+      }
+
+      for(var i = 0; i < routes.length; i++){
+        var replaced = false;
+
+        if(routes[i].location.label === route.location.label){
+          replaced = true;
+          routes[i] = route;
+          break;
+        }
+      }
+
+      if(!replaced){
+        routes.push(route);
+      }
+
+      addToSavedLocations(route);
+      removeLoadingItem();
+
+    }
+  });
 }
 
 getLocations();
